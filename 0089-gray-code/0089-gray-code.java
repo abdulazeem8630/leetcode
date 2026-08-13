71a62b49c93f22829560f4b99897644674ccd643
Abdul Azeem <abdulazeem863062@gmail.com>
2026-08-13 19:53:20 +0530

    Time: 7 ms (13.98%), Space: 53.9 MB (10.46%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -4,9 +4,12 @@ import java.util.List;
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<>();
-        int totalNumbers = 1 << n; 
-        for (int i = 0; i < totalNumbers; i++) {
-            result.add(i ^ (i >> 1));
+        result.add(0);
+        for (int i = 0; i < n; i++) {
+            int mask = 1 << i;
+            for (int j = result.size() - 1; j >= 0; j--) {
+                result.add(result.get(j) | mask);
+            }
         }
         return result;
     }
