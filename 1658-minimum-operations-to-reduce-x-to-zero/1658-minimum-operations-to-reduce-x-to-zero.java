1f35c6fc571217fb6e3e9417d0647c9e0d83f666
Abdul Azeem <abdulazeem863062@gmail.com>
2026-09-23 18:37:28 +0530

    Time: 53 ms (14.39%), Space: 144.9 MB (12.33%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int target = -x;
+        for (int num : nums) {
+            target += num;
+        }
+
+        if (target < 0) return -1;
+        if (target == 0) return nums.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - target)) {
+                maxLen = Math.max(maxLen, i - map.get(sum - target));
+            }
+            map.put(sum, i);
+        }
+
+        return maxLen == -1 ? -1 : nums.length - maxLen;
+    }
+}
