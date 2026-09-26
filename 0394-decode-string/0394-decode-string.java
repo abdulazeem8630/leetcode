a0303501780b10d17664f9061c201361bbf25715
Abdul Azeem <abdulazeem863062@gmail.com>
2026-09-26 08:08:41 +0530

    Time: 1 ms (84.24%), Space: 42.9 MB (60.22%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                int repeatTimes = countStack.pop();
+                for (int j = 0; j < repeatTimes; j++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+
+        return currentString.toString();
+    }
+}
